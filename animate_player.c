bb704e522775c776806ab1b2615c7d340266075c
leontinepaq <lpaquatt@made-f0Ar3s8.clusters.42paris.fr>
2024-01-05 18:00:25 +0100

    debut animation saut et deplacements...

--- animate_player.c
+++ animate_player.c
@@ -6,62 +6,11 @@
 /*   By: lpaquatt <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 15:22:16 by lpaquatt          #+#    #+#             */
-/*   Updated: 2024/01/05 14:54:49 by lpaquatt         ###   ########.fr       */
+/*   Updated: 2024/01/05 17:44:35 by lpaquatt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/so_long.h"
-
-/*
-void	anim_landing(t_var *vars)
-{
-	vars->game->player->anim_frame = 6;
-	render_img(vars);
-	usleep(500000);
-	vars->game->player->anim_frame = 7;
-	render_img(vars);
-	usleep(500000);
-	vars->game->player->movement = NORMAL;
-	vars->game->player->anim_frame = 0;
-}
-
-void	gravity(t_var *vars)
-{
-	int		x;
-	int 	y;
-	char	tile_bellow;
-
-	if (vars->game->player->position->pos_on_tile == POS_UP)
-	{
-		usleep (200000);
-		vars->game->player->position->pos_on_tile = POS_CENTER;
-		return ;
-	}
-	if (vars->game->player->position->pos_on_tile == POS_CENTER)
-	{
-		x = vars->game->player->position->x_tile;
-		y = vars->game->player->position->y_tile + 1;
-		tile_bellow = vars->map->tiles[y][x];
-		if (tile_bellow == '0' || tile_bellow == 'C')
-		{
-			usleep (150000);
-			vars->game->player->position->pos_on_tile = POS_UP;
-			move_player(vars, 0, 1);
-			vars->game->player->movement = JUMP;
-			vars->game->player->anim_frame = 5;
-		}
-		else
-		{
-			vars->game->player->movement = NORMAL;
-			vars->game->player->anim_frame = 0;
-		}
-		/ *
-			if (vars->game->player->movement == JUMP)
-				anim_landing(vars);
-		* /
-	}
-}
-*/
 
 void	animate_normal(t_var *vars, t_anim *player, t_map *map)
 {
@@ -74,46 +23,97 @@ void	animate_normal(t_var *vars, t_anim *player, t_map *map)
 	{
 		move_player(vars, 0, 1);
 		player->position->pos_on_tile = POS_UP;
-//		player->movement = FALL;
-		player->movement = NORMAL;
+		player->movement = FALL;
 		player->anim_frame = 0;
 	}
 	else
 		player->anim_frame += 2 * ((player->anim_frame + 1) % 2) - 1;
 }
 
-void	animate_walk(t_var *vars, t_anim *player, t_map *map)
+void	animate_jump(t_var *vars, t_anim *player, t_map *map)
 {
 	(void) vars;
 	(void) map;
 	player->anim_frame += 1;
+	if (player->anim_frame == 2)
+		player->position->pos_on_tile = POS_UP;
+	else if (player->anim_frame == 3)
+	{
+		player->position->pos_on_tile = POS_CENTER;
+		player->position->y_tile -= 1;
+	}
+	else if (player->anim_frame == 4)
+	{
+		player->movement = FALL;
+		player->anim_frame = 0;
+	}
+}
+
+void	animate_walk(t_var *vars, t_anim *player, t_map *map)
+{
+	(void) vars;
+	(void) map;
+	
+	player->anim_frame += 1;
 	if (player->anim_frame == 1)
-		player->position->pos_on_tile = POS_RIGHT;
-	else if (player->anim_frame == 2)
 	{
 		player->position->pos_on_tile = POS_LEFT;
 		player->position->x_tile += 1;
 	}
-	else if (player->anim_frame == 3)
+	else if (player->anim_frame == 2)
 		player->position->pos_on_tile = POS_CENTER;
-	else if (player->anim_frame == 4)
+	else if (player->anim_frame == 3)
 	{
 		player->movement = NORMAL;
 		player->anim_frame = 0;
+		animate_normal(vars, player, map);
 	}
 	else if (player->anim_frame == 5)
-		player->position->pos_on_tile = POS_LEFT;
-	else if (player->anim_frame == 6)
 	{
 		player->position->pos_on_tile = POS_RIGHT;
 		player->position->x_tile -= 1;
 	}
-	else if (player->anim_frame == 7)
+	else if (player->anim_frame == 6)
 		player->position->pos_on_tile = POS_CENTER;
-	else if (player->anim_frame == 8)
+	else if (player->anim_frame == 7)
 	{
 		player->movement = NORMAL;
 		player->anim_frame = 2;
+		animate_normal(vars, player, map);
+	}
+}
+
+void	animate_fall(t_var *vars, t_anim *player, t_map *map)
+{
+	char	tile_bellow;
+
+	tile_bellow = map->tiles[player->position->y_tile + 1][player->position->x_tile];
+	if (player->position->pos_on_tile == POS_UP 
+		|| player->position->pos_on_tile == POS_LEFT
+		|| player->position->pos_on_tile == POS_RIGHT)
+	{
+		player->position->pos_on_tile = POS_CENTER;
+		player->direction = DIR_DOWN;
+	}
+	else if (tile_bellow == '0' || tile_bellow == 'C' )
+	{
+		move_player(vars, 0, 1);
+		player->position->pos_on_tile = POS_UP;
+		player->movement = FALL;
+		player->anim_frame = 0;
+		player->direction = DIR_DOWN;
+	}
+	else
+	{
+		player->anim_frame += 1;
+		if (player->anim_frame == 4)
+		{
+			player->movement = NORMAL;
+			if (player->direction == DIR_LEFT)
+				player->anim_frame = 2;
+			else
+				player->anim_frame = 0;
+		}
 	}
 }
 
@@ -130,28 +130,47 @@ void	animate_player(t_var *vars)
 	{
 		vars->game->player->timestamp = current_timestamp;
 		if (vars->game->player->direction == DIR_LEFT)
+		{
 			vars->game->player->position->x_tile +=1;
+			vars->game->player->position->pos_on_tile = POS_LEFT;
+		}
 		if (vars->game->player->direction == DIR_RIGHT)
+		{
 			vars->game->player->position->x_tile -=1;
+			vars->game->player->position->pos_on_tile = POS_RIGHT;
+		}
 		if (vars->game->player->direction == DIR_UP)
+		{
 			vars->game->player->position->y_tile +=1;
-		animate_walk(vars, vars->game->player, vars->map);
+		}
+		vars->game->player->timestamp = current_timestamp;
 	}
-	if (current_timestamp - vars->game->player->timestamp < 300)
+	if (current_timestamp - vars->game->player->timestamp < 200)
 		return ;
 	if (vars->game->player->movement == WALK)
 	{
 		animate_walk(vars, vars->game->player, vars->map);
 		vars->game->player->timestamp = current_timestamp;
 	}
-	if (current_timestamp - vars->game->player->timestamp < 1000)
+	if (vars->game->player->movement == JUMP)
+	{
+		animate_jump(vars, vars->game->player, vars->map);
+		vars->game->player->timestamp = current_timestamp;
+	}
+		if (vars->game->player->movement == FALL)
+	{
+		animate_fall(vars, vars->game->player, vars->map);
+		vars->game->player->timestamp = current_timestamp;
+	}
+	if (current_timestamp - vars->game->player->timestamp < 700)
 		return ;
 	if (vars->game->player->movement == NORMAL)
+	{
 		animate_normal(vars, vars->game->player, vars->map);
+		vars->game->player->timestamp = current_timestamp;
+	}
 //	ft_printf("movement : %d - dir : %d - anim_frame : %d ", vars->game->player->movement, vars->game->player->direction, vars->game->player->anim_frame);
 //	ft_printf("- pos tile : %d - x_tile : %d\n", vars->game->player->position->pos_on_tile,  vars->game->player->position->x_tile);
-	vars->game->player->timestamp = current_timestamp;
-	return ;
 }
 
 /*
